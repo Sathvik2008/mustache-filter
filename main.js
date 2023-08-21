e8154ee4a00b98984a65425d3979075fa3bc7cfc
Sathvik2008 <109099815+Sathvik2008@users.noreply.github.com>
2023-08-21 00:09:15 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -3,8 +3,24 @@ function preload(){
 function setup(){
 canvas=createCanvas(500,500);
 canvas.center();
+video=createCapture(VIDEO);
+video.size(500,500);
+video.hide();
+poseNet=ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
+}
+function modelLoaded(){
+console.log("Posenet is initialized")
+}
+function gotPoses(){
+if(results.length>0){
+console.log(results);
+console.log("Nose x="+results[0].pose.nose.x);
+console.log("Nose y="+results[0].pose.nose.y);
+}
 }
 function draw(){
+image(video, 0, 0, 500, 500);
 }
 function take_snapshot(){
 save('mustache.png');
