2afd11d2bd69b99a857ac4eb900c468b956c44b1
Sathvik2008 <109099815+Sathvik2008@users.noreply.github.com>
2023-08-21 23:55:42 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
+noseX=0;
+noseY=0;
 function preload(){
+mustache=loadImage('https://i.postimg.cc/3x3QzSGq/m.png');
 }
 function setup(){
 canvas=createCanvas(500,500);
@@ -12,15 +15,18 @@ poseNet.on('pose', gotPoses);
 function modelLoaded(){
 console.log("Posenet is initialized")
 }
-function gotPoses(){
+function gotPoses(results){
 if(results.length>0){
 console.log(results);
-console.log("Nose x="+results[0].pose.nose.x);
-console.log("Nose y="+results[0].pose.nose.y);
+noseX=results[0].pose.nose.x-12;
+noseY=results[0].pose.nose.y+10;
+console.log("Nose x="+noseX);
+console.log("Nose y="+noseY);
 }
 }
 function draw(){
 image(video, 0, 0, 500, 500);
+image(mustache, noseX, noseY, 30, 30);
 }
 function take_snapshot(){
 save('mustache.png');
